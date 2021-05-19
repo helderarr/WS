c9cd9e7e0f60e32781ca33f5345a12654ab5f58b
Helder Rodrigues <helderarrodrigues@gmail.com>
2021-05-19 21:31:23 +0200

    add evaluayion data

--- pipeline_test.py
+++ pipeline_test.py
@@ -6,6 +6,9 @@ from PassageReader import PassageReader
 from SumarizerStep import SumarizerStep
 from TopNPassages import TopNPassages
 from interfaces import Pipeline
+import pandas as pd
+
+pd.set_option('mode.chained_assignment', None)
 
 passage_reader = PassageReader()
 
@@ -13,26 +16,31 @@ pipeline_dbpedia = Pipeline()
 pipeline_dbpedia.add_step(DbPediaSpotlightEntityExtractor())
 pipeline_dbpedia.add_step(PageRankPassageRanker())
 pipeline_dbpedia.add_step(TopNPassages(3))
-pipeline_dbpedia.add_step(SumarizerStep(100,200))
+pipeline_dbpedia.add_step(SumarizerStep(100, 200))
 
 pipeline_blink = Pipeline()
 pipeline_blink.add_step(DbPediaSpotlightAnnotator())
 pipeline_blink.add_step(BlinkEntityExtractor())
 pipeline_blink.add_step(PageRankPassageRanker())
 pipeline_blink.add_step(TopNPassages(3))
-pipeline_blink.add_step(SumarizerStep(100,200))
-
+pipeline_blink.add_step(SumarizerStep(100, 200))
 
 utterances = passage_reader.data["conversation_utterance_id"]
 utterances = utterances.drop_duplicates()
-
 
 for utt in list(utterances[1:-1]):
 
     passages = passage_reader.get_utterance_passages(utt)
 
     out1 = pipeline_dbpedia.run(passages)
-    print(out1)
+    # print(out1)
 
     out2 = pipeline_blink.run(passages)
-    print(out2)
+    # print(out2)
+
+    if not out2 == out1:
+        print("================================")
+        print(out1)
+        print("================================")
+        print(out2)
+        print("================================")
