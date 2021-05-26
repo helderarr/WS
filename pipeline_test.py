0e21c062a71c897c9ef17b5cc6796d508659911d
Helder Rodrigues <helderarrodrigues@gmail.com>
2021-05-26 22:03:07 +0200

    disable blink

--- pipeline_test.py
+++ pipeline_test.py
@@ -1,5 +1,5 @@
 from DbPediaSpotlightAnnotator import DbPediaSpotlightAnnotator
-from BlinkEntityExtractor import BlinkEntityExtractor
+#from BlinkEntityExtractor import BlinkEntityExtractor
 from DbPediaSpotlightEntityExtractor import DbPediaSpotlightEntityExtractor
 from PageRankPassageRanker import PageRankPassageRanker
 from PassageReader import PassageReader
@@ -8,7 +8,9 @@ from TopNPassages import TopNPassages
 from interfaces import Pipeline
 import pandas as pd
 
+
 pd.set_option('mode.chained_assignment', None)
+
 
 passage_reader = PassageReader()
 
@@ -18,12 +20,12 @@ pipeline_dbpedia.add_step(PageRankPassageRanker())
 pipeline_dbpedia.add_step(TopNPassages(3))
 pipeline_dbpedia.add_step(SumarizerStep(100, 200))
 
-pipeline_blink = Pipeline()
-pipeline_blink.add_step(DbPediaSpotlightAnnotator())
-pipeline_blink.add_step(BlinkEntityExtractor())
-pipeline_blink.add_step(PageRankPassageRanker())
-pipeline_blink.add_step(TopNPassages(3))
-pipeline_blink.add_step(SumarizerStep(100, 200))
+#pipeline_blink = Pipeline()
+#pipeline_blink.add_step(DbPediaSpotlightAnnotator())
+#pipeline_blink.add_step(BlinkEntityExtractor())
+#pipeline_blink.add_step(PageRankPassageRanker())
+#pipeline_blink.add_step(TopNPassages(3))
+#pipeline_blink.add_step(SumarizerStep(100, 200))
 
 utterances = passage_reader.data["conversation_utterance_id"]
 utterances = utterances.drop_duplicates()
@@ -35,12 +37,13 @@ for utt in list(utterances[1:-1]):
     out1 = pipeline_dbpedia.run(passages)
     # print(out1)
 
-    out2 = pipeline_blink.run(passages)
+    #out2 = pipeline_blink.run(passages)
     # print(out2)
 
-    if not out2 == out1:
-        print("================================")
-        print(out1)
-        print("================================")
-        print(out2)
-        print("================================")
+    #if not out2 == out1:
+    #    print("================================")
+    #    print(out1)
+    #    print("================================")
+    #    print(out2)
+    #    print("================================")
+#
